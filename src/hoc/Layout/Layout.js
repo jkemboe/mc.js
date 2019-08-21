8a2f1ef1b9899b988cc48b11b980882e695a35ad
maasai <jkemboe@gmail.com>
2019-08-21 16:05:49 +0300

    feat: change UI on Landing page, login, and registration

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -4,12 +4,20 @@ import React from 'react'
 
 const Layout = ({ children }) => {
   return (
-    <div
-      style={{
-        backgroundImage: 'linear-gradient(to top, #30cfd0 0%, #330867 100%)'
-      }}
-      className={classes.layout__wrapper}
-    >
+    <div className={classes.layout__wrapper}>
+      {[1, 2, 3, 4, 5].map(keyb => {
+        if (keyb % 2 === 0) {
+          return <div className={classes.layout__animation} />
+        }
+        if (keyb % 3 === 0) {
+          return <div className={classes.layout__animation_left} />
+        }
+        if (keyb % 5 === 0) {
+          return <div className={classes.layout__animations_right} />
+        }
+        return <div className={classes.layout__animations_far_right} />
+      })}
+
       {children}
     </div>
   )
